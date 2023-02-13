b678f9abc4c586b4bde31fe43e7a15329c9179f9
Murilo Santori Boff <murilosantori45@gmail.com>
2023-02-13 10:37:49 -0300

    Aprendendo JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = "Murilo"
+
+  function sum(a,b) {
+    return a+b
+  }
+
+  const img = 'https://placeholder.com/150'
+
   return (
     <div className="App">
       <h1>Ola React</h1>
-      <p>Meu primeiro App</p>
+      <p>Ola {name}</p>
+      <p>Soma {2+2}</p>
+      <p>Soma {sum(8,7)}</p>
+      <img alt="Imagem" src={img}/>
     </div>
   );
 }
