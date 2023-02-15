1f1430fa46eed7a8da1733f43baf2aac41518f46
Murilo Santori Boff <murilosantori45@gmail.com>
2023-02-15 10:09:23 -0300

    Atribuindo Props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = "Murilo"
-
-  function sum(a,b) {
-    return a+b
-  }
-
-  const img = 'https://placeholder.com/150'
-
-  return (
-    <div className="App">
-      <h1>Ola React</h1>
-      <p>Ola {name}</p>
-      <p>Soma {2+2}</p>
-      <p>Soma {sum(8,7)}</p>
-      <img alt="Imagem" src={img}/>
-      <HelloWorld/>
-    </div>
+return (
+  <div className="App">
+    <HelloWorld/>
+    <SayMyName nome="Joao"/>
+    <Pessoa nome="Andre" idade="20" profissao="Marceneiro" foto="https://via.placeholder.com/150"/>
+  </div>
   );
 }
 
---
+++ meu-projeto/src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props) {
+    return (
+        <div>
+            <p>Fala ai {props.nome}, td certo?</p>    
+        </div>
+    )
+}
+
+export default SayMyName
