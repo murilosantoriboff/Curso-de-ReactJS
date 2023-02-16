53bb3ad108993960148ed2f200afc619da2ae0df
Murilo Santori Boff <murilosantori45@gmail.com>
2023-02-16 06:54:30 -0300

    CSS modules

--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -1,6 +1,8 @@
+import styles from './Frase.module.css'
+
 function Frase() {
     return (
-        <div>
+        <div className={styles.fraseContainer}>
             <p>Este é um componente Frase</p>
         </div>
     )
