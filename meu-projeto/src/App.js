a5d52720900bda2e5f5869768a927a4db5f3659a
Murilo Santori Boff <murilosantori45@gmail.com>
2023-02-25 07:49:21 -0300

    Renderizacao de Lista

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+  const meusItens = ['React', 'Vue', 'Angular']
+
 return (
   <div className="App">
-    <h1>Testando condicional</h1>
-    <Condicional/>
+    <h1>Testando de Listas</h1>
+    <OutraLista itens={meusItens}/>
+    <OutraLista itens={[]}/>
   </div>
   );
 }
