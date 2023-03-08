8430897e45f09a25d3730ea9436d60c0170f9564
Murilo Santori Boff <murilosantori45@gmail.com>
2023-03-08 19:50:15 -0300

    Navbar e footer p1

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,20 +1,17 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </ul>
+      <Navbar/>
       <Container customClass='min-height'>
       <Routes>
           <Route exact path='/' element={<Home/>}>
@@ -32,7 +29,7 @@ function App() {
           
       </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
---
+++ costs/src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import {Link} from 'react-router-dom'
+
+import Container from './Container';
+
+import styles from './Navbar.module.css';
+import logo from '../../img/costs_logo.png'
+
+function Navbar() {
+    return (
+        <nav>
+            <Container>
+                <Link to='/'>
+                    <img src={logo} alt='Costs'/>
+                </Link>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contato</Link>
+                    </li>
+                    <li>
+                        <Link to='/company'>Empresa</Link>
+                    </li>
+                </ul>
+            </Container>
+        </nav>
+    )
+}
+
+export default Navbar
