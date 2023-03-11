61675e9ff6af60815fa06d74c96b02bf013fddc6
Murilo Santori Boff <murilosantori45@gmail.com>
2023-03-11 13:27:47 -0300

    Finalizacao da page Home

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,7 +1,16 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <section className={styles.home_container}>
+                <h1>Bem-vindo ao <span>Costs</span></h1>
+                <p>Começe a gerenciar os seus projetos agora mesmo!</p>
+                <LinkButton text='Criar Projetos' to='/newproject'/>
+                <img src={savings} alt='Costs'/>
+            </section>
         </div>
     )
 }
